396f360e2bc5565fe8eb23cda81e6e456aaf9fc2
phamtuan7592 <phamtuan7592@gmail.com>
2025-05-04 00:34:03 +0700

    tuan

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,41 +1,48 @@
 import { createContext, useEffect, useState } from "react";
-import { doctors } from "../assets/assets";
 import axios from 'axios'
 import { toast } from "react-toastify"
 
 export const AppContext = createContext()
 
-const AppContextProvider = (props) =>{
-    
+const AppContextProvider = (props) => {
+
     const currencySymbol = '$'
     const backendUrl = import.meta.env.VITE_BACKEND_URL
-    const[doctors,setDoctors] = useState([])
 
-    const value = {
-        doctors, 
-        currencySymbol
-    }
-    const getDoctorsData = async () =>{
-        try{
-            const {data} =await axios.get(backendUrl + '/api/doctor/list')
-            if(data.success){
+    const [doctors, setDoctors] = useState([])
+    const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token'):false)
+
+
+
+    const getDoctorsData = async () => {
+        try {
+            const { data } = await axios.get(backendUrl + '/api/doctor/list')
+            if (data.success) {
                 setDoctors(data.doctors)
-            }else{
+            } else {
                 toast.error(data.message)
             }
 
-        } catch (error){
+        } catch (error) {
             console.log(error)
             toast.error(error.message)
         }
     }
-    useEffect(()=>{
-        getDoctorsData()
-    },[])
 
-    return(
+    const value = {
+        doctors,
+        currencySymbol,
+        token, setToken,
+        backendUrl
+    }
+
+    useEffect(() => {
+        getDoctorsData()
+    }, [])
+
+    return (
         <AppContext.Provider value={value}>
-            {props.children}  
+            {props.children}
         </AppContext.Provider>
     )
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+
+  const { backendUrl, token, setToken } = useContext(AppContext)
+  const navigate = useNavigate()
 
   const [state, setState] = useState('Sign Up')
 
@@ -11,29 +18,61 @@ const Login = () => {
   const onSubmitHandler = async (event) => {
     event.preventDefault()
 
+    try {
 
+      if (state === 'Sign Up') {
+
+        const { data } = await axios.post(backendUrl + '/api/user/register', { name, password, email })
+        if (data.success) {
+          localStorage.setItem('token', data.token)
+          setToken(data.token)
+        } else {
+          toast.error(data.message)
+        }
+
+      } else {
+
+        const { data } = await axios.post(backendUrl + '/api/user/login', { password, email })
+        if (data.success) {
+          localStorage.setItem('token', data.token)
+          setToken(data.token)
+        } else {
+          toast.error(data.message)
+        }
+
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
 
+  useEffect(() => {
+    if (token){
+      navigate('/')
+    }
+  },[token])
+
   return (
-    <form className='min-h-[80vh] flex items-center'>
+    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
       <div className='flex flex-col gap-5 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
         <p className='text-2xl font-semibold'>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
         <p>Please {state === 'Sign Up' ? "sign up" : "log in"} to book appointment</p>
         {
           state === "Sign Up" && <div className='w-full'>
             <p>Full Name</p>
-            <input className='border border-zinc-400 rounded w-full p-2 mt-1' type="text"value={name} onChange={(e) => setName(e.target.value)}  required />
+            <input className='border border-zinc-400 rounded w-full p-2 mt-1' type="text" value={name} onChange={(e) => setName(e.target.value)} required />
           </div>
         }
         <div className='w-full'>
           <p>Email</p>
-          <input className='border border-zinc-400 rounded w-full p-2 mt-1' type="email" value={email} onChange={(e) => setEmail(e.target.value)}  required />
+          <input className='border border-zinc-400 rounded w-full p-2 mt-1' type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
         </div>
         <div className='w-full'>
           <p>Password</p>
           <input className='border border-zinc-400 rounded w-full p-2 mt-1' type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
         </div>
-        <button className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+        <button type='submit' className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
         {
           state === "Sign Up"
             ? <p>Already have an account? <span onClick={() => setState('Login')} className='text-primary underline cursor-pointer'>Login here</span></p>
