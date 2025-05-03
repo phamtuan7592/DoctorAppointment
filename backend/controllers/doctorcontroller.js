79d4cb010f1675b9680bb45861c64696c9059f60
NguyenMinhCanh1001 <2251120402@ut.edu.vn>
2025-05-03 21:30:17 +0700

    update

--- backend/controllers/doctorcontroller.js
+++ backend/controllers/doctorcontroller.js
@@ -16,4 +16,15 @@ const changeAvailablity = async(req,res)=>{
         res.json({success:false, message:error.message})
     }
 }
-export {changeAvailablity}
+const doctorList = async(req,res)=>{
+    try{
+        const doctors = await doctorModel.find({}).select(['-password','-email'])
+
+        res.json({success:true,doctors})
+    }catch(error){
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
+
+export {changeAvailablity,doctorList}
